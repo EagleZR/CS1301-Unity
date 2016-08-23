461f4bd87f8e0df63eac45ac1568e1e57c92cc71
Mark Zeagler <mark.zeagler@outlook.com>
2016-08-22 23:25:49 -0400

    Initial Set-up
    Combined the 2 repositories into 1

---
+++ MonoDevelop/CSE 1301/Lab01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab01
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			string input = Console.ReadLine ();
+			Console.Write ("You input: ");
+			Console.WriteLine (input);
+
+			Console.WriteLine ("Please input a number:");
+			string input1 = Console.ReadLine ();
+			int input1a = 0;
+			while(Int32.TryParse (input1, out input1a)){ // Still works if a word is submitted. Hmmmmm......
+				Console.WriteLine ("That is not a number. Please input a number:"); //TODO does this work? YAY! :D
+				input1 = Console.ReadLine ();
+				input1a = 0;
+			}
+
+			Console.WriteLine ("Please input a word:");
+			string input2 = Console.ReadLine ();
+
+			TestObject testObject = new TestObject (input1a, input2);
+
+			Console.Write ("You input the number ");
+			Console.Write (testObject.getNumber ());
+			Console.Write (" and the word ");
+			Console.WriteLine (testObject.getWords ());
+		}
+	}
+}
---
+++ MonoDevelop/CSE 1301/Lab01/TestObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab01
+{
+	public class TestObject
+	{
+		private int number;
+		private String words;
+
+		public TestObject ()
+		{
+		}
+
+		public TestObject (int number, string words){
+			this.number = number;
+			this.words = words;
+		}
+			
+		public int getNumber(){
+			return this.number;
+		}
+
+		public string getWords(){
+			return this.words;
+		}
+
+	}
+}
+
