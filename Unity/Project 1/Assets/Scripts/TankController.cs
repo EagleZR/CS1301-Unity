bd5d22e1a938399e429b2296ea7fc62a115df647
Mark Zeagler <mark.zeagler@outlook.com>
2016-09-07 11:22:11 -0400

    Miscellaneous work
    Work I did in class.

--- Unity/Project 1/Assets/Scripts/TankController.cs
+++ Unity/Project 1/Assets/Scripts/TankController.cs
@@ -27,10 +27,11 @@ public class TankController : MonoBehaviour {
 		 * }
 		 * */
 		Vector3 rotation = Vector3.RotateTowards (transform.forward, target.position - transform.position, rotateSpeed * Time.deltaTime, 0.0f);
-		Vector3 checkRotation = transform.rotation.eulerAngles;
-		print (rotation.y.ToString() + " , " + checkRotation.y.ToString());
-		if (checkRotation.y != rotation.y) {
+		// Vector3 checkRotation = transform.rotation.eulerAngles;
+		// print (rotation.y.ToString() + " , " + checkRotation.y.ToString());
+		if (rotation.y <= 0.01f) {
 			transform.forward = rotation; 
+			print (target.position - transform.position);
 		} else {
 			print ("Ready to move");
 		}
