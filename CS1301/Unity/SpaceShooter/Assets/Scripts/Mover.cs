cb3e95abb518b089da5fb12d63622f29075f19a2
Mark Zeagler <mark.zeagler@outlook.com>
2016-10-17 15:15:51 -0400

    Reorganizing Master
    I did this in a branch, but I forgot to do it here

---
+++ CS1301/Unity/SpaceShooter/Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = gameObject.GetComponent<Rigidbody> ();
+		rb.velocity = transform.forward * speed;
+	}
+}
---
+++ CS1301/Unity/SpaceShooter/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+	
+	public float tumble;
+
+	private Rigidbody rb;
+
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody> ();
+		rb.angularVelocity = Random.insideUnitSphere * tumble; 
+	}
+}
