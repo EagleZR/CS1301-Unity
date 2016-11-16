a35e0accaff6002a1b60713212c4a80bcadf3db5
Mark Zeagler <mark.zeagler@outlook.com>
2016-11-15 21:26:26 -0500

    Dev 4
    Added some camera rig functionality and added to the terrace templates

---
+++ CS1301/Unity/Project 3/Assets/Scripts/Game 3 Scripts/CameraRotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotator : MonoBehaviour {
+
+	public float rotateSpeed;
+
+	void Start () {
+	
+	}
+	
+	void FixedUpdate () {
+		transform.Rotate( new Vector3( 0, this.rotateSpeed, 0 ) );
+	}
+}
---
+++ CS1301/Unity/Project 3/Assets/Scripts/Game 3 Scripts/CameraSwitcher.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CameraSwitcher : MonoBehaviour {
+
+	public float delaySwitch;
+	public List<GameObject> cameras;
+
+	private float currTime; // check for an internal Unity version of this
+	private GameObject cameraOn;
+
+	void Start () {
+		foreach ( GameObject camera in cameras ) {
+			camera.SetActive( false );
+		}
+		this.currTime = this.delaySwitch;
+		this.cameraOn = this.cameras[0];
+		PickNewCamera();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		this.currTime -= Time.deltaTime;
+		if ( this.currTime <= 0 ) {
+			PickNewCamera();
+			this.currTime = this.delaySwitch;
+		}
+	}
+
+	private void PickNewCamera () {
+		this.cameraOn.SetActive( false );
+		int newCamera = (int)Random.Range( 0, this.cameras.Count );
+		this.cameraOn = this.cameras[newCamera];
+		this.cameraOn.SetActive( true );
+		print( newCamera );
+	}
+}
